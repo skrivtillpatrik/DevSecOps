efd256e38ece206d73eedf22e526cd6c8bb2976c
Patrik Jellerbo <skrivtillpatrik@gmail.com>
2026-04-25 12:50:32 +0200

    vitest ska inte köra playwrigt filer

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+     exclude: [
+      "tests/e2e/**",
+      "node_modules/**"
+    ],
     globals: true,
     environment: "node",
     setupFiles: ["./tests/setupTestDB.js"]
